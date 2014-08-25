36f5518a995efb0198f43409d5e4c035f93fee8f
danialbleile <danial.bleile@wsu.edu>
2014-08-25 16:19:18 -0700

    Added Cycle.js - will probably remove this

--- controls/script_control.php
+++ controls/script_control.php
@@ -10,6 +10,7 @@ class script_control {
 	public function wp_scripts(){
 		wp_enqueue_style( 'cahnrs_core_css', URL.'css/cahnrs-core-css.css' );
 		wp_enqueue_script( 'cahnrs_core_js', URL . '/js/cahnrs-core-js.js' );
+		wp_enqueue_script( 'cycle_js',  URL . '/js/cycle.js', array(), '1.0.0', true );
 	}
 	
 	public function wp_admin_scripts(){
